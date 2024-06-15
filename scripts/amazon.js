251130d3e08ccc21c5769e91a2809c8e3ac1c0cc
Jivansh77 <jivansh777@gmail.com>
2024-06-15 17:09:30 +0530

    Displaying 'added' when adding to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -37,7 +37,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added</div>
 
@@ -47,6 +47,8 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+const addedMessageTimeouts = {};
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
@@ -80,6 +82,21 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       cartQuantity+=item.quantity;
     });
 
-      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    const previousTimeoutId = addedMessageTimeouts[productId];
+    if (previousTimeoutId) 
+    {
+      clearTimeout(previousTimeoutId);
+    }
+    const timeoutId = setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+
+  addedMessageTimeouts[productId] = timeoutId;
   });
 });
