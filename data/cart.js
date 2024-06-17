b2a759fb3fb8ecb617a401d96961414cfd681b0d
Jivansh77 <jivansh777@gmail.com>
2024-06-18 00:24:23 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+} , {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
+//just some data for understanding how it works, avtually it will be empty.
 
 export function addToCart(productId)
 {
