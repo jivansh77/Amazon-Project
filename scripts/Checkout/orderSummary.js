6ce315928980d49e16c667577f20b45dc211b17d
Jivansh77 <jivansh777@gmail.com>
2024-06-23 03:02:22 +0530

    When deleting a product, make orderSummary interactive

--- scripts/Checkout/orderSummary.js
+++ scripts/Checkout/orderSummary.js
@@ -94,10 +94,9 @@ export function renderOrderSummary()
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
       updateCartQuantity();
 
+      renderOrderSummary();
       renderPaymentSummary();
     });
   }); 
