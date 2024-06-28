37c23d57a8e1b6d900575f5da72052c7333b2265
Jivansh77 <jivansh777@gmail.com>
2024-06-28 19:51:38 +0530

    Create test suite updateDeliveryOption

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,4 @@
+import {validDeliveryOption} from './deliveryOptions.js';
 export let cart;
 
 loadFromStorage();
@@ -112,6 +113,17 @@ export function updateDeliveryOption(productId, deliveryOptionId)
     if (productId === cartItem.productId) 
     matchingItem = cartItem;
   });
+
+  if (!matchingItem) 
+  {
+    return;
+  }
+  
+  if (!validDeliveryOption(deliveryOptionId)) 
+  {
+    return;
+  }
+
   matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
