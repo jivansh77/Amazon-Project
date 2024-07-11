63f39718e43fb042386e7217d8f001e283fd5568
Jivansh77 <jivansh777@gmail.com>
2024-07-12 02:25:00 +0530

    Added class Appliance to products that have instructions & warranty

--- data/products.js
+++ data/products.js
@@ -64,6 +64,27 @@ class Clothing extends Product
   }
 }
 
+class Appliance extends Product 
+{
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) 
+  {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() 
+  {
+    return `<a href="${this.instructionsLink}" target="_blank">
+        Instructions</a>
+        <a href="${this.warrantyLink}" target="_blank">
+        Warranty</a>`;
+  }
+}
+
 /*
 const date = new Date();
 console.log(date);
@@ -149,7 +170,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -334,7 +358,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -639,7 +666,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -699,7 +729,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -780,6 +813,10 @@ export const products = [
   if(productDetails.type === 'clothing')
   {
     return new Clothing(productDetails);
+  }
+  else if(productDetails.type === 'appliance')
+  {
+    return new Appliance(productDetails);
   }
   return new Product(productDetails);
   //loops through array and returns new array where each value is a product class instead of a regular object
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {renderCheckoutHeader} from "./Checkout/checkoutHeader.js";
 import {renderOrderSummary} from "./Checkout/orderSummary.js";
 import {renderPaymentSummary} from "./Checkout/paymentSummary.js";
 //import '../data/cart-class.js';
-import '../data/car.js';
+//import '../data/car.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
