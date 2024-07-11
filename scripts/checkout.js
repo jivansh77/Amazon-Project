8e4559d39a78fe19303557f4d312d7f9d3e9bba0
Jivansh77 <jivansh777@gmail.com>
2024-07-11 21:39:57 +0530

    Adding class Car and its objects for practise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
 import {renderCheckoutHeader} from "./Checkout/checkoutHeader.js";
 import {renderOrderSummary} from "./Checkout/orderSummary.js";
 import {renderPaymentSummary} from "./Checkout/paymentSummary.js";
-//import '../data/cart-class.js'
+//import '../data/cart-class.js';
+import '../data/car.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
