f1deffc7fc565812e58ed45310b309e29626c0b5
Jivansh77 <jivansh777@gmail.com>
2024-07-16 20:28:00 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,12 +2,52 @@ import {renderCheckoutHeader} from "./Checkout/checkoutHeader.js";
 import {renderOrderSummary} from "./Checkout/orderSummary.js";
 import {renderPaymentSummary} from "./Checkout/paymentSummary.js";
 import {loadProducts} from "../data/products.js";
+import {loadCart} from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/car.js';
 //import '../data/backend-practise.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+]).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-});  //first backend should receive request then run the code
+});
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    }); //need to call another promise to wait for cart to load
+  });
+}).then(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  }); //first backend should receive request then run the code
+});
+*/
