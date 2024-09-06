0ede9b219e395a72a0452fad485559b31f2d5812
Jivansh77 <jivansh777@gmail.com>
2024-09-07 02:12:14 +0530

    Making the tracking page

--- data/orders.js
+++ data/orders.js
@@ -10,3 +10,15 @@ function saveToStorage()
 {
   localStorage.setItem('orders', JSON.stringify(orders));
 }
+
+export function getOrder(orderId) 
+{
+  let matchingOrder;
+  orders.forEach((order) => {
+    if (order.id === orderId) 
+    {
+      matchingOrder = order;
+    }
+  });
+  return matchingOrder;
+}
